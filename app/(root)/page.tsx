6b078eb84ec8968afd1ace657990517494aa48ab
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-23 00:22:31 +0600

    bookoverview

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,18 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 import Image from "next/image";
 
 const Home = () => {
   return (
     <>
-      <BookOverview></BookOverview>
-      <BookList></BookList>
+      <BookOverview {...sampleBooks[0]}></BookOverview>
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      ></BookList>
     </>
   );
 };
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const BookOverview = () => {
+const BookOverview = ({
+  title,
+  author,
+  genre,
+  rating,
+  total_copies,
+  available_copies,
+  description,
+  color,
+  cover,
+}:Book) => {
   return (
-    <section className='book-overview'>
-        <div className='flex flex-1 flex-col gap-5'>
-            <h1>A long Book Title</h1>
-        </div>
-    </section>
-  )
-}
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>{title}</h1>
 
-export default BookOverview
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+
+          <p>
+            Category{" "}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+
+          <div className="flex flex-row gap-1">
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+        </div>
+
+        <div className="book-copies">
+          <p>
+            Total Books <span>{total_copies}</span>
+          </p>
+
+          <p>
+            Available Books <span>{available_copies}</span>
+          </p>
+        </div>
+
+        <p className="book-description">{description}</p>
+      </div>
+    </section>
+  );
+};
+
+export default BookOverview;
