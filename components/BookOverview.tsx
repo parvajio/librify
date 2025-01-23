0737c4176398fe0b0cebd36919097fdcccd80df8
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-23 20:50:48 +0600

    book cover added in the hero section

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
+import BookCover from "./BookCover";
 
 const BookOverview = ({
   title,
@@ -11,12 +13,11 @@ const BookOverview = ({
   description,
   color,
   cover,
-}:Book) => {
+}: Book) => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
-
         <div className="book-info">
           <p>
             By <span className="font-semibold text-light-200">{author}</span>
@@ -32,7 +33,6 @@ const BookOverview = ({
             <p>{rating}</p>
           </div>
         </div>
-
         <div className="book-copies">
           <p>
             Total Books <span>{total_copies}</span>
@@ -42,8 +42,33 @@ const BookOverview = ({
             Available Books <span>{available_copies}</span>
           </p>
         </div>
-
         <p className="book-description">{description}</p>
+
+        <Button className="book-overview_btn">
+          <Image src="/icons/book.svg" alt="book" width={20} height={20} />
+          <p className="font-bebas-neue text-xl text-dark-100">
+            {"Borrow Book"}
+          </p>
+        </Button>
+      </div>
+
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+          <BookCover
+            variant="wide"
+            className="z-10"
+            coverColor={color}
+            coverImage={cover}
+          ></BookCover>
+
+          <div className="absolute left-16 top-10 rotate-12 opacity-40">
+            <BookCover
+              variant="wide"
+              coverColor={color}
+              coverImage={cover}
+            ></BookCover>
+          </div>
+        </div>
       </div>
     </section>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      }
+    ]
+  }
   /* config options here */
+
 };
 
 export default nextConfig;
