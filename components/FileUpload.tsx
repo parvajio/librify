9004a8f1d55a07e0497e0a8bf7532b66deedf28a
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-25 22:34:08 +0600

    imagekit installetion and config set up for keys

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,4 +1,15 @@
-import React from 'react'
+"use client";
+
+import { IKImage, IKUpload, ImageKitContext } from "imagekitio-next";
+
+const authenticator = async() =>{
+  try {
+
+    
+  }catch(error: any){
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+}
 
 const FileUpload = () => {
   return (
---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    eng:{
+        imagekit: {
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+          },
+      
+    }
+}
+
+export default config;
