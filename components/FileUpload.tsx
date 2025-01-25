3e7e8d7fee2b0359ae41a10a1d22991d588e1fb8
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-25 22:56:06 +0600

    imagekit route create

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,20 +1,32 @@
 "use client";
 
+import config from "@/lib/config";
 import { IKImage, IKUpload, ImageKitContext } from "imagekitio-next";
 
-const authenticator = async() =>{
+const authenticator = async () => {
   try {
+    const res = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
 
-    
-  }catch(error: any){
+    if (!res.ok) {
+      const errorText = await res.text();
+
+      throw new Error(
+        `Request failed with status ${res.status}: ${errorText}`
+      );
+    }
+
+    const data = await res.json();
+
+    const {signature, expire,token} = data;
+
+    return {signature, expire, token}
+  } catch (error: any) {
     throw new Error(`Authentication request failed: ${error.message}`);
   }
-}
+};
 
 const FileUpload = () => {
-  return (
-    <div>FileUpload</div>
-  )
-}
+  return <div>FileUpload</div>;
+};
 
-export default FileUpload
+export default FileUpload;
