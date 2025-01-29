8abccb599541209641c7639d1a298c0bd715a24c
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-29 22:58:40 +0600

    sign in function

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,39 +1,57 @@
 "use server"
 
+import { signIn } from "@/auth";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
 
-const signup = async (params: AuthCredentials) =>{
-    const {fullName, email, universityId, universityCard, password} = params;
+const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
+    const { email, password } = params;
 
-    const existingUser= await db
-    .select()
-    .from(users)
-    .where(eq(users.email, email))
-    .limit(1)
+    try {
+        const result = await signIn("credentials", { email, password, redirect: false })
 
-    if(existingUser.length > 0){
-        return {success: false, error : "user already exist "}
+        if (result?.error){
+            return { success : false, error : result.error}
+        }
+
+        return {success : true}
+    } catch (error) {
+        console.log(error, "sign in error")
+        return { success: false, error: "Sign in error" }
+    }
+}
+
+const signup = async (params: AuthCredentials) => {
+    const { fullName, email, universityId, universityCard, password } = params;
+
+    const existingUser = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email))
+        .limit(1)
+
+    if (existingUser.length > 0) {
+        return { success: false, error: "user already exist " }
     }
 
     const hashedPassword = await hash(password, 10)
 
-    try{
+    try {
         await db.insert(users).values({
             fullName,
             email,
             universityId,
-            password= hashedPassword,
+            password: hashedPassword,
             universityCard
         });
 
-        // await signInWithCredentials({email, password})
+        await signInWithCredentials({ email, password })
 
-        return {success :true}
-    }catch(err){
+        return { success: true }
+    } catch (err) {
         console.log(err, "sign up error")
-        return {success: false, error: "signup error"}
+        return { success: false, error: "signup error" }
     }
 }
