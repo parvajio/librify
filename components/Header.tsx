c27b61d8ff36769cc16062bfc3221b87fdc1d7b0
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-31 22:41:20 +0600

    used shadcn avater

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,52 @@
-"use client"
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 const Header = () => {
-
   const pathname = usePathname();
 
   return (
-    <header className='my-10 flex justify-between gap-5'>
-      <Link href={'/'}>
-        <Image src={'/icons/logo.svg'} alt='logo' width={40} height={40}></Image>
+    <header className="my-10 flex justify-between gap-5">
+      <Link href={"/"}>
+        <Image
+          src={"/icons/logo.svg"}
+          alt="logo"
+          width={40}
+          height={40}
+        ></Image>
       </Link>
 
-      <ul className='flex flex-row items-center gap-8'>
+      <ul className="flex flex-row items-center gap-8">
         <li>
-          <Link href={'/library'} className={cn('text-base cursor-pointer capitalize', pathname === '/library'? 'text-light-200': 'text-light-100')}>
+          <Link
+            href={"/library"}
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
             Library
           </Link>
         </li>
         <li>
           <Link href={"/my-profile"}>
-            
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
           </Link>
         </li>
       </ul>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
