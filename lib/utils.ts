67d3c36722c2ad1fa4b3426176e08b978e427eff
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-31 23:13:51 +0600

    f

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,4 +5,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const getInitials = (name: string ): string => name.split(" ").map(n=>n[0]).join("").toUpperCase().slice(0,2);
