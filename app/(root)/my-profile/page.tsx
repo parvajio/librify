51cf34663e40bedf096be56b748ca4f9fe3b79ea
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-01 11:10:04 +0600

    Borrowed booklist in my profile

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,17 +1,30 @@
-import { signOut } from '@/auth'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { signOut } from "@/auth";
+import BookList from "@/components/BookList";
+import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
+import React from "react";
 
 const page = () => {
   return (
-    <form action={async () => {
-        "use server"
+    <>
+      <form
+        action={async () => {
+          "use server";
 
-        await signOut();
-    }} className='mb-10'>
+          await signOut();
+        }}
+        className="mb-10"
+      >
         <Button>Log Out</Button>
-    </form>
-  )
-}
+      </form>
 
-export default page
+      <BookList
+        title="Borrowed Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      ></BookList>
+    </>
+  );
+};
+
+export default page;
