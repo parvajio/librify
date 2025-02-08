cfb60e3d48614a917588184dda222c0ad83febf8
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-08 13:13:41 +0600

    backaground change

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,28 +1,25 @@
-import { auth } from '@/auth'
-import Header from '@/components/Header'
-import { redirect } from 'next/navigation';
-import React, { ReactNode } from 'react'
+import { auth } from "@/auth";
+import Header from "@/components/Header";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
 
-const layout = async({children }: {children : ReactNode}) => {
-
-  const session  = await auth();
-  if(!session){
-    redirect("/sign-in")
+const layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  if (!session) {
+    redirect("/sign-in");
   }
 
   return (
-    <main className='root-container'>
-        <div className='mx-auto max-w-7xl'>
-            <Header session = {session}>
-                
-            </Header>
+    <main className="root-container">
+      <div className="bg-black !w-full backdrop-blur-sm bg-opacity-10 px-5 sm:px-10 md:px-16">
+          <div className="mx-auto max-w-7xl">
+            <Header session={session}></Header>
 
-            <div className='mt-20 pb-20'>
-                {children}
-            </div>
-        </div>
+            <div className="mt-20 pb-20">{children}</div>
+          </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
