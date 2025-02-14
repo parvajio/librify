60c10741d4f20f1bba4762dea4b5e4c2582e8460
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-14 10:33:13 +0600

    color picker input

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
-import { HexColorPicker } from "react-colorful";
+import { HexColorInput, HexColorPicker } from "react-colorful";
 
 interface props {
-    value? : string;
-    onPickerChange : (color: string) => void;
+  value?: string;
+  onPickerChange: (color: string) => void;
 }
 
-const ColorPicker = ({value,onPickerChange}) => {
-  const [color, setColor] = useState("#aabbcc");
-  const [isOpen, setIsOpen] = useState(false)
-  return <HexColorPicker color={color} onChange={setColor} />;
+const ColorPicker = ({ value, onPickerChange }: props) => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center">
+        <p>#</p>
+        <HexColorInput
+          color={value}
+          onChange={onPickerChange}
+          className="hex-input"
+        ></HexColorInput>
+      </div>
+      <HexColorPicker color={value} onChange={onPickerChange} />
+    </div>
+  );
 };
 
 export default ColorPicker;
