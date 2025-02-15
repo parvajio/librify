5dbcbc3c06c34c99741fcf91ed908e8639a99f24
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-15 14:10:47 +0600

    bd generate and migrate

--- components/admin/forms/BookForm.tsx
+++ components/admin/forms/BookForm.tsx
@@ -18,6 +18,8 @@ import { Textarea } from "@/components/ui/textarea";
 import FileUpload from "@/components/FileUpload";
 import ColorPicker from "@/components/ColorPicker";
 import { Button } from "@/components/ui/button";
+import { createBook } from "@/lib/admin/actions/book";
+import { toast } from "@/hooks/use-toast";
 
 interface props extends Partial<Book> {
   type?: "create" | "update";
@@ -43,7 +45,22 @@ const BookForm = ({ type, ...book }: props) => {
   });
 
   const onSubmit = async (values: z.infer<typeof bookSchema>) => {
-    console.log(values);
+    const result = await createBook(values);
+
+    if(result.success){
+      toast({
+        title: "Success",
+        description: "Book created Successfully"
+      })
+
+      router.push(`/admin/books/${result.data.id}`);
+    }else{
+      toast({
+        title: "Error",
+        description : result.message,
+        variant: 'destructive'
+      })
+    }
   };
 
   return (
