9470c6a95652276fc8134ea107a1039ef21acec9
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-02-18 01:51:23 +0600

    check is eligible or not for borrowing and show error message

--- components/BorrowBook.tsx
+++ components/BorrowBook.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
+import { useRouter } from 'next/navigation';
+import { toast } from '@/hooks/use-toast';
 
 interface Props {
     userId : string;
@@ -13,7 +15,34 @@ interface Props {
     }
 }
 
-const BorrowBook = ({userId, bookId, borrowingEligibility}: Props) => {
+const BorrowBook = ({userId, bookId, borrowingEligibility: {isEligible, message}}: Props) => {
+    const router = useRouter();
+    const [borrowing, setBorrowing] = useState(false);
+
+    const handleBorrow = async ()=>{
+        if(!isEligible){
+            toast({
+                title: "Error",
+                description: message,
+                variant: "destructive"
+            })
+        }
+
+        setBorrowing(true);
+
+        try {
+            
+        } catch (error) {
+            toast({
+                title: "Error",
+                description: "An error occured while borrowing the book",
+                variant: "destructive"
+            })
+        } finally{
+            setBorrowing(false)
+        }
+    }
+
   return (
     <Button className="book-overview_btn">
           <Image src="/icons/book.svg" alt="book" width={20} height={20} />
