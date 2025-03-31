207edfc94080a4cd7a7ffdda36e967bc544d225d
parvajio <mdparvajmosharof17@gmail.com>
2025-03-31 20:07:02 +0600

    pagination functionality applied

--- app/(root)/library/page.tsx
+++ app/(root)/library/page.tsx
@@ -1,16 +1,52 @@
-import { db } from '@/database/drizzle'
-import { books } from '@/database/schema'
-import React from 'react'
+import BookList from "@/components/BookList";
+import { db } from "@/database/drizzle";
+import { books } from "@/database/schema";
+import { cn } from "@/lib/utils";
+import { sql } from "drizzle-orm";
+import React from "react";
 
-const page = async () => {
+const page = async ({ searchParams }: { searchParams: { page?: string } }) => {
+  const PAGE_SIZE = 15;
+  const params = await searchParams;
+  const currentPage = params.page ? parseInt(params.page, 10) : 1;
+  const offset = (currentPage - 1) * PAGE_SIZE;
 
-    const allBooks = await db.select().from(books);
-    console.log(allBooks)
+  const allBooks = await db
+    .select()
+    .from(books)
+    .limit(PAGE_SIZE)
+    .offset(offset);
+
+  const totalBook = await db
+    .select({ count: sql<number>`COUNT(*)` })
+    .from(books);
+  // console.log(totalBook);
+  const totalPages = Math.ceil(Number(totalBook[0].count) / PAGE_SIZE);
+  // console.log(totalPages);
+
   return (
-    <div>
+    <section className="text-white">
       This is the library page
-    </div>
-  )
-}
+      <BookList
+        title="Books"
+        books={allBooks}
+        containerClassName="mt-28"
+      ></BookList>
 
-export default page
+      {/* pagination */}
+      <div className="flex gap-2">
+        {
+          Array.from({length: totalPages}, (v,i)=>(
+            <div key={i}>
+              <a href={`?page=${i+1}`}>
+                {i+1}
+              </a>
+            </div>
+          ))
+        }
+      </div>
+    </section>
+  );
+};
+
+export default page;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React from "react";
 // import { auth, signIn, signOut } from "@/auth";
 import { Button } from "./ui/button";
 import { SiWikibooks } from "react-icons/si";
 import { Avatar, AvatarFallback } from "./ui/avatar";
-import { getInitials } from "@/lib/utils";
-import { useRouter } from "next/navigation";
+import { cn, getInitials } from "@/lib/utils";
+import { usePathname, useRouter } from "next/navigation";
 import { Session } from "next-auth";
 
-const Header = ({session}:{session:Session}) => {
-  const router = useRouter()
+const Header = ({ session }: { session: Session }) => {
+  const router = useRouter();
+  const pathname = usePathname();
 
   const handleSignIn = () => {
-    router.push("/sign-in")
-  }
+    router.push("/sign-in");
+  };
 
   return (
     <header className="my-10 flex justify-between gap-5">
@@ -25,30 +26,8 @@ const Header = ({session}:{session:Session}) => {
       </Link>
 
       <ul className="flex flex-row items-center gap-8">
-        <li>
-          {/* <Link
-            href={"/library"}
-            className={cn(
-              "text-base cursor-pointer capitalize",
-              pathname === "/library" ? "text-light-200" : "text-light-100"
-            )}
-          >
-            Library
-          </Link> */}
-        </li>
         {session ? (
           <li>
-            {/* <form
-              action={async () => {
-                // "use server";
-
-                await signOut();
-              }}
-              className="mb-10"
-            >
-              <Button>Log Out</Button>
-            </form> */}
-
             <Link href={"/my-profile"}>
               <Avatar>
                 <AvatarFallback className="bg-amber-100">
@@ -58,9 +37,22 @@ const Header = ({session}:{session:Session}) => {
             </Link>
           </li>
         ) : (
-          <li>
-            <Button onClick={handleSignIn}>Sign In</Button>
-          </li>
+          <>
+            <li>
+              <Link
+                href={"/library"}
+                className={cn(
+                  "text-base cursor-pointer capitalize",
+                  pathname === "/library" ? "bg-primary text-primary-foreground shadow hover:bg-primary/90 p-2 px-4 rounded-lg" : "text-light-100"
+                )}
+              >
+                Library
+              </Link>
+            </li>
+            <li>
+              <Button onClick={handleSignIn}>Sign In</Button>
+            </li>
+          </>
         )}
       </ul>
     </header>
