2d67fab5564445d7f7eba70cbc3007e8f36934fe
parvajio <mdparvajmosharof17@gmail.com>
2025-04-04 19:21:03 +0600

    similer books in book details

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,52 +1,71 @@
-import { auth } from '@/auth';
-import BookOverview from '@/components/BookOverview';
-import BookVideo from '@/components/BookVideo';
-import { db } from '@/database/drizzle';
-import { books } from '@/database/schema';
-import { eq } from 'drizzle-orm';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { auth } from "@/auth";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import BookVideo from "@/components/BookVideo";
+import { db } from "@/database/drizzle";
+import { books } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = async({params}: {params: Promise<{id: string}>}) => {
-    const id = (await params).id;
-    const session = await auth()
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const session = await auth();
 
-    // fatch data based on id 
-    const [bookDetails] = await db.select().from(books).where(eq(books.id, id)).limit(1);
-    
-    if(!bookDetails){
-        redirect("/404");
-    }
+  // fatch data based on id
+  const [bookDetails] = await db
+    .select()
+    .from(books)
+    .where(eq(books.id, id))
+    .limit(1);
 
-    // console.log(bookDetails)
+  if (!bookDetails) {
+    redirect("/404");
+  }
+
+  const { genre } = bookDetails;
+  const similarBook = await db
+    .select()
+    .from(books)
+    .where(eq(books.genre, genre));
 
   return (
     <>
-        <BookOverview {...bookDetails} userId={session?.user?.id as string}></BookOverview>
+      <BookOverview
+        {...bookDetails}
+        userId={session?.user?.id as string}
+      ></BookOverview>
 
-        <div className='book-details'>
-            <div className='flex-[1.5]'>
-                <section className='flex flex-col gap-7'>
-                    <h3>Video</h3>
-                    <BookVideo videoUrl={bookDetails.videoUrl}></BookVideo>
-                </section>
-                
-                {/* <summary></summary> */}
+      <div className="book-details">
+        <div className="flex-[1.5]">
+          <section className="flex flex-col gap-7">
+            <h3>Video</h3>
+            <BookVideo videoUrl={bookDetails.videoUrl}></BookVideo>
+          </section>
 
-                <section className="mt-10 flex flex-col gap-7">
-                    <h3>Summary</h3>
-                    <div className='space-y-5 text-xl text-lime-100'>
-                        {bookDetails.summary.split("\n").map((line, i) =>(
-                            <p key={i}>{line}</p>
-                        ))}
-                    </div>
-                </section>
+          {/* <summary></summary> */}
+
+          <section className="mt-10 flex flex-col gap-7">
+            <h3>Summary</h3>
+            <div className="space-y-5 text-xl text-lime-100">
+              {bookDetails.summary.split("\n").map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
             </div>
-
-            {/* similar books */}
+          </section>
         </div>
-    </>
-  )
-}
 
-export default page
+        {/* similar books */}
+        <div className="flex-1">
+          <BookList
+            title="Similer Books"
+            books={similarBook}
+            containerClassName=""
+          ></BookList>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
